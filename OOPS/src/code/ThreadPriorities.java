b71e9e8d04d6d11584bc4d65c763b5dcfa34b3fb
Farazulhaque <farazattari786@gmail.com>
2021-10-30 12:11:46 +0530

    priorities in thread in java

---
+++ OOPS/src/code/ThreadPriorities.java
@@ -0,0 +1,39 @@
+package code;
+
+class MyThr1 extends Thread {
+    public MyThr1(String name) {
+        super(name);
+    }
+
+    public void run() {
+        while (true) {
+            // System.out.println("I am a thread");
+            System.out.println("Thank you: " + this.getName());
+        }
+
+    }
+}
+
+public class ThreadPriorities {
+    public static void main(String[] args) {
+        // Ready Queue: T1 T2 T3 T4 T5
+        MyThr1 t1 = new MyThr1("Faraz");
+        MyThr1 t2 = new MyThr1("Faraz2");
+        MyThr1 t3 = new MyThr1("Faraz3");
+        MyThr1 t4 = new MyThr1("Faraz4");
+        MyThr1 t5 = new MyThr1("Faraz5 (most Important)");
+        t5.setPriority(Thread.MAX_PRIORITY);
+        t1.setPriority(Thread.MIN_PRIORITY);
+        t2.setPriority(Thread.MIN_PRIORITY);
+        t3.setPriority(Thread.MIN_PRIORITY);
+        t4.setPriority(Thread.MIN_PRIORITY);
+        t5.setPriority(Thread.MIN_PRIORITY);
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+
+    }
+}

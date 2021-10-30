dbad8b45dcfb51b674c719b9fa29e1d7a4887931
Farazulhaque <farazattari786@gmail.com>
2021-10-30 10:39:39 +0530

    using threading in java

---
+++ OOPS/src/code/MultiThreading.java
@@ -0,0 +1,35 @@
+package code;
+
+class MyThread1 extends Thread {
+    @Override
+    public void run() {
+        int i = 0;
+        while (i < 40000) {
+            System.out.println("A with F");
+            System.out.println("I am happy!");
+            i++;
+        }
+    }
+}
+
+class MyThread2 extends Thread {
+    @Override
+    public void run() {
+        int i = 0;
+        while (i < 40000) {
+            System.out.println("A without F");
+            System.out.println("I am sad!");
+            i++;
+        }
+    }
+}
+
+public class MultiThreading {
+    public static void main(String[] args) {
+        MyThread1 t1 = new MyThread1();
+        MyThread2 t2 = new MyThread2();
+        t1.start();
+        t2.start();
+
+    }
+}
